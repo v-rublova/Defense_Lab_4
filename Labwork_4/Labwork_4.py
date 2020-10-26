25203db33e81bd07b2b8ff002910d64b5528cdf2
v.rublova <v.rublova@student.csn.khai.edu>
2020-10-26 18:41:44 +0200

    Added input and output

--- Labwork_4/Labwork_4.py
+++ Labwork_4/Labwork_4.py
@@ -1,50 +1,76 @@
 from sympy import *
 import libnum
 import hashlib
-import random
 
 class elg_:
 
     data = ['p', 'g', 'y', 'x']
 
     def sign(self, M, K):
-        c_1 = pow(self.g, K, self.p)
+        c_1 = pow(self.g, K, self.p) #c_1 = (g^k) mod p
+        #M is hash; c_2=(M*((y^k) mod p)) mod p
         c_2 = (int.from_bytes(M,"big") * pow(self.y, K, self.p)) % self.p
         return (c_1, c_2)
 
     def verify(self, M, sig):
+        '''
+M - hash;
+sig - signature [c_1,c_2]
+    	'''
+        #whereas M=(c_2/(c_1^x)) mod p put a tremendous strain on my CPU a
+    	#roundabound way was found
+        #starting from here
         r = randprime(2, self.p - 1)
-        a_blind = (sig[0] * pow(self.g, r, self.p)) % self.p
-        ax = pow(a_blind, self.x, self.p)
-        plaintext_blind = (sig[1] * libnum.invmod(ax, self.p)) % self.p
-        val_1 = (plaintext_blind * pow(self.y, r, self.p)) % self.p    
+        b = (sig[0] * pow(self.g, r, self.p)) % self.p
+        ax = pow(b, self.x, self.p)
+        buf = (sig[1] * libnum.invmod(ax, self.p)) % self.p
+        val_1 = (buf * pow(self.y, r, self.p)) % self.p
+        #to over here
+        print("Received hash:",val_1,sep="\n")
         val_2 = int.from_bytes(M,"big")
+        #Comparison of received and calculated
         if val_1 == val_2:
             return 1
         return 0
 
 def keys(floor,ceil):
-    obj = elg_()
-    obj.g = randprime(floor,ceil)
-    obj.p = randprime(floor,ceil)
+
+    obj = elg_() #object, which contains ['p', 'g', 'y', 'x']
+    obj.g = randprime(floor,ceil) #random prime number in range [floor,ceil]
+    obj.p = randprime(floor,ceil) #random prime number in range [floor,ceil]
     if (obj.g > obj.p):
-        obj.g,obj.p = obj.p,obj.g 
+        obj.g,obj.p = obj.p,obj.g #if g is greater then p they swap
+
+    print("*" * 10,"p=" + str(obj.p),"g=" + str(obj.g),"*" * 10,sep="\n")
+
     # Generate private key x
     obj.x = randprime(2, obj.p - 1)
     # Generate public key y
     obj.y = pow(obj.g, obj.x, obj.p)
+    print("*" * 10,"y=" + str(obj.y),"*" * 10,sep="\n")
     return obj
 
-bit_size = 512
-message = "Hi"
-key = keys(pow(10,bit_size - 1),pow(10,bit_size))
+size = 256
+#if message is not null
+t = true
+#message
+while t:
+    message = input("Your message:")
+    if message: t = false
 hash = hashlib.sha512(message.encode("utf8")).digest()
-print(hash)
-while 1:
-    k = randprime(pow(10,bit_size - 1),key.p - 1)
-    if gcd(k,key.p - 1) == 1: break
-signature = key.sign(hash,k)
-if key.verify(hash,signature):
-    print("OK")
+if (len(str(int.from_bytes(hash,"big"))) > size):
+    print("Message can't be signed with current 'size' value.")
 else:
-    print("Incorrect signature")
+    key = keys(pow(10,size - 1),pow(10,size))
+
+    print("hash:",int.from_bytes(hash,"big"),sep="\n")
+    while 1:
+        k = randprime(pow(10,size - 1),key.p - 1)
+        if gcd(k,key.p - 1) == 1: break
+    print("*" * 10,"k=" + str(k),"*" * 10,sep="\n")
+    signature = key.sign(hash,k)
+    print("C1:",signature[0],"C2:",signature[1],sep="\n")
+    if key.verify(hash,signature):
+        print("Data is OK")
+    else:
+        print("Error")
